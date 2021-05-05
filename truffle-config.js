d7da83a795ed9462f5ce3ee1b2ae87b5191cf954
Nathan Rowe <nathandrowe1@gmail.com>
2021-05-05 15:48:12 -0500

    v2.0.0 FULL CRYPTO COIN

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+require("dotenv").config({path: "./.env"});
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const AccountIndex = 0;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -8,7 +11,25 @@ module.exports = {
     development: {
       port: 7545,
       host: "127.0.0.1",
+      network_id: 5777 // Match any network id  
+    },
+    ganache_local: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:7545", AccountIndex)
+      },
       network_id: 5777
+    },
+    goerli: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, "https://goerli.infura.io/v3/40659b3b12cb48cfbc53b05fcadd1eb4", AccountIndex)
+      },
+      network_id: 5
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/40659b3b12cb48cfbc53b05fcadd1eb4", AccountIndex)
+      },
+      network_id: 3
     }
   },
   compilers: {
